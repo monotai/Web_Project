61390e68958d3a32d8d9e69923adeeab235efa36
monotai <wannwattqarl@gmail.com>
2025-04-01 14:34:11 +0700

    change

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
   return (
     <> 
     <div className='Container'>
-    <Level_6 />
+    <Level_7 />
     </div>
       {/* <div className="Container">
         {currentLevel === 7 && <Level_7 onComplete={() => handleLevelComplete(7)} />}
--- project/src/JSX/Level_7.jsx
+++ project/src/JSX/Level_7.jsx
@@ -6,7 +6,7 @@ function Game_7({ code = "" }) {
     return (
         <>
             <div className="Game">
-                <div className="center">
+                <div className="center" style={{fontSize: "1.5rem"}}>
                     {code === correctCode 
                         ? "Correct! Props are passed as attributes in JSX." 
                         : code 
@@ -36,41 +36,30 @@ function Level_7({ onComplete }) {
   
     return (
       <>
-        <div className="InputFeedbackContainer">
-          {/* Left Section: Description and Input */}
-          <div className="LeftSection">
-            <div className="Description">
-              <h1 className="webName">React Quiz - Level 7</h1>
-              <p>
-                Help the component display the correct title by passing the `title` prop!<br />
-                Complete the code below:
-              </p>
-            </div>
-            <div className="InputSection">
-              <pre>
-                {`<MyComponent `}
-                <input
-                  value={code}
-                  onChange={(e) => setCode(e.target.value)}
-                  onKeyPress={handleKeyPress} // Listen for Enter key
-                  placeholder="Type the missing code here..."
-                />
-                {` />`}
-              </pre>
-            </div>
-          </div>
-  
-          {/* Right Section: Feedback */}
-          <div className="RightSection">
-            <div className="FeedbackSection">
-              {code === correctCode
-                ? "🎉 Correct! The title prop has been passed successfully."
-                : code
-                ? "❌ Incorrect! Try again."
-                : "Waiting for the correct title..."}
-            </div>
+        {/* Left Section: Description and Input */}
+        <div className="Description">
+          <h1 className="webName">React Quiz - Level 7</h1>
+          
+          <div className="code">
+            <p>
+              Help the component display the correct title by passing the `title` prop!<br />
+              Complete the code below:
+            </p>
+            <pre>
+              {`<MyComponent `}
+              <input
+                value={code}
+                onChange={(e) => setCode(e.target.value)}
+                onKeyPress={handleKeyPress} // Listen for Enter key
+                placeholder="Type the missing code here..."
+              />
+              {` />`}
+            </pre>
           </div>
         </div>
+
+        {/* Right Section: Feedback */}
+        <Game_7 code={code} />
       </>
     );
   }
